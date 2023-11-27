7d3bb88f609364d9d424f39824eac5ddbabb2372
josuerx12 <josueaze12@gmail.com>
2023-11-27 17:48:25 -0300

    feat: add bootstrap to build the front end.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
+import { Routes } from "react-router-dom";
 import Footer from "./components/footer";
 import Header from "./components/header";
-import Sidebar from "./components/sidebar";
 
 const App = () => {
   return (
     <div className="app">
       <Header />
-      <main className="content">
-        <Sidebar />
-        <section>Teste 2</section>
-      </main>
+      <Routes></Routes>
       <Footer />
     </div>
   );
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,34 +1,109 @@
-import {
-  NavWrapper,
-  NavLogo,
-  NavList,
-  NavListItems,
-  MobileButton,
-} from "./styles";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Offcanvas from "react-bootstrap/Offcanvas";
 import { RiShieldUserFill, RiLogoutBoxRLine } from "react-icons/ri";
-import { RiMenuFoldFill } from "react-icons/ri";
+import { useNavigate } from "react-router-dom";
+import { FaPlusCircle, FaUsers } from "react-icons/fa";
+import { IoGitPullRequestSharp } from "react-icons/io5";
+import { MdOutlineDashboardCustomize } from "react-icons/md";
+import { TiCancel } from "react-icons/ti";
+import { GoPackageDependents } from "react-icons/go";
 
 const Header = () => {
-  return (
-    <NavWrapper>
-      <NavLogo>AlmoxSystem</NavLogo>
-      <NavList>
-        <li>
-          <NavListItems to="/login">
-            <p>Entrar</p> <RiShieldUserFill />
-          </NavListItems>
-        </li>
-        <li>
-          <NavListItems to="/">
-            <p>Sair</p> <RiLogoutBoxRLine />
-          </NavListItems>
-        </li>
-      </NavList>
+  const navigate = useNavigate();
+  function handleNavigate(e: React.FormEvent, path: string) {
+    e.preventDefault();
+    navigate(path);
+  }
 
-      <MobileButton>
-        <RiMenuFoldFill />
-      </MobileButton>
-    </NavWrapper>
+  return (
+    <Navbar key="xxl" expand="xxl" className="mb-3 bg-primary">
+      <Container fluid>
+        <Navbar.Brand
+          className="text-white"
+          onClick={(e) => handleNavigate(e, "/")}
+        >
+          AlmoxSystem
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="offcanvasNavbar-expand-xxl" />
+        <Navbar.Offcanvas
+          id={`offcanvasNavbar-expand-"xxl"`}
+          aria-labelledby="offcanvasNavbarLabel-expand-xxl"
+          placement="end"
+          className="bg-primary"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title
+              id="offcanvasNavbarLabel-expand-xxl"
+              onClick={(e) => handleNavigate(e, "/")}
+              className="text-white"
+            >
+              AlmoxSystem
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <NavDropdown
+                title="Gerenciar"
+                id="offcanvasNavbarDropdown-expand-xxl"
+                menuVariant="dark"
+              >
+                <NavDropdown.Item
+                  onClick={(e) => handleNavigate(e, "/admin/users")}
+                >
+                  Usuários <FaUsers />
+                </NavDropdown.Item>
+                <NavDropdown.Item
+                  onClick={(e) => handleNavigate(e, "/admin/requests")}
+                >
+                  Solicitações <IoGitPullRequestSharp />
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown
+                title="Almoxarifado"
+                id="offcanvasNavbarDropdown-expand-xxl"
+                menuVariant="dark"
+              >
+                <NavDropdown.Item
+                  onClick={(e) => handleNavigate(e, "/almox/dashboard")}
+                >
+                  Dashboard <MdOutlineDashboardCustomize />
+                </NavDropdown.Item>
+                <NavDropdown.Item
+                  onClick={(e) => handleNavigate(e, "/almox/canceledRequests")}
+                >
+                  Solicitações Canceladas <TiCancel />
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown
+                title="Solicitações"
+                id="offcanvasNavbarDropdown-expand-xxl"
+                menuVariant="dark"
+              >
+                <NavDropdown.Item
+                  onClick={(e) => handleNavigate(e, "/solicitacao/nova")}
+                >
+                  Nova Solicitação <FaPlusCircle />
+                </NavDropdown.Item>
+                <NavDropdown.Item
+                  onClick={(e) => handleNavigate(e, "/solicitacao/acompanhar")}
+                >
+                  Acompanhar Solicitações <GoPackageDependents />
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link onClick={(e) => handleNavigate(e, "/login")}>
+                Entrar <RiShieldUserFill />
+              </Nav.Link>
+              <Nav.Link onClick={(e) => handleNavigate(e, "/logout")}>
+                Sair <RiLogoutBoxRLine />
+              </Nav.Link>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
   );
 };
 
